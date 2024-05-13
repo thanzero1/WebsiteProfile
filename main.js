1b982c3b192a59ebf3ff69a316d8629716a8e814
Nathan Garcia <nathangarcia750@gmail.com>
2024-05-13 00:16:25 -0300

    Correção de Bugs

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const linkInterruptor = document.querySelector("#interruptor");
+let box = document.querySelector("#checkbox");
+let interruptor = document.querySelector("#interruptor");
+
+interruptor.addEventListener("click", function () {
+  box.checked = !box.checked;
+  changeMode();
+});
+
+function changeMode() {
+  let changesList = document.querySelectorAll(".change");
+  let BgchangesList = document.querySelectorAll(".BgChange");
+
+ 
+  let i = 0;
+
+  for (i = 0; i < changesList.length; i++) {
+    let itemChange = changesList[i];
+    
+    
+    if (box.checked) {
+      itemChange.style.color = "#FFF";
+      interruptor.innerHTML = "Ascender as Luzes";
+    } else {
+      itemChange.style.color = "#000001";
+      interruptor.innerHTML = "Apagar as Luzes";
+    }
+  };
+  
+  for (i = 0; i < BgchangesList.length; i++) {
+    let BgChange = BgchangesList[i];
+    
+    if (box.checked) {
+      BgChange.style.backgroundColor = "#000001";
+    } else {
+      BgChange.style.backgroundColor = "#FFF";
+    }
+  };
+   
+}
+ 
