35cd6a05eb0d98b01d931d5d708fcbdbe41214b3
Nathan Garcia <nathangarcia750@gmail.com>
2025-03-05 18:12:51 -0300

    Correção de Bugs

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const linkInterruptor=document.querySelector("#interruptor");let box=document.querySelector("#checkbox"),interruptor=document.querySelector("#interruptor");function changeMode(){var r=document.querySelectorAll(".change"),o=document.querySelectorAll(".BgChange");let t=0;for(t=0;t<r.length;t++){let e=r[t];box.checked?(e.style.color="#FFF",interruptor.innerHTML="Ascender as Luzes"):(e.style.color="#000001",interruptor.innerHTML="Apagar as Luzes")}for(t=0;t<o.length;t++){let e=o[t];box.checked?e.style.backgroundColor="#000001":e.style.backgroundColor="#FFF"}}interruptor.addEventListener("click",function(){box.checked=!box.checked,changeMode()});
+const linkInterruptor = document.querySelector("#interruptor");
+let box = document.querySelector("#checkbox"),
+  interruptor = document.querySelector("#interruptor");
+function changeMode() {
+  var r = document.querySelectorAll(".change"),
+    o = document.querySelectorAll(".BgChange");
+  let t = 0;
+  for (t = 0; t < r.length; t++) {
+    let e = r[t];
+    box.checked
+      ? ((e.style.color = "#F9F6EE"),
+        (interruptor.innerHTML = "Mudar Tema"))
+      : ((e.style.color = "##353935"));
+        // (interruptor.innerHTML = "Apagar as Luzes"));
+  }
+  for (t = 0; t < o.length; t++) {
+    let e = o[t];
+    box.checked
+      ? (e.style.backgroundColor = "#353935")
+      : (e.style.backgroundColor = "#F9F6EE");
+  }
+}
+interruptor.addEventListener("click", function () {
+  (box.checked = !box.checked), changeMode();
+});
