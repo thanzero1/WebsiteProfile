7dbc93c8fdd78a338ae32a42af05dd4edf984b9f
Nathan Garcia <nathangarcia750@gmail.com>
2025-03-31 01:48:37 -0300

    Correção de Bugs

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const linkInterruptor = document.querySelector("#interruptor");
 let box = document.querySelector("#checkbox"),
   interruptor = document.querySelector("#interruptor");
+
+
+
 function changeMode() {
   var r = document.querySelectorAll(".change"),
     o = document.querySelectorAll(".BgChange");
@@ -8,10 +11,9 @@ function changeMode() {
   for (t = 0; t < r.length; t++) {
     let e = r[t];
     box.checked
-      ? ((e.style.color = "#FAF9F6"),
-        (interruptor.innerHTML = "Mudar Tema"))
-      : ((e.style.color = "#1c1c1c"));
-        // (interruptor.innerHTML = "Apagar as Luzes"));
+      ? ((e.style.color = "#FAF9F6"), (interruptor.innerHTML = "Mudar Tema"))
+      : (e.style.color = "#1c1c1c");
+    // (interruptor.innerHTML = "Apagar as Luzes"));
   }
   for (t = 0; t < o.length; t++) {
     let e = o[t];
@@ -23,3 +25,5 @@ function changeMode() {
 interruptor.addEventListener("click", function () {
   (box.checked = !box.checked), changeMode();
 });
+
+
