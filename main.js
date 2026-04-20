ac72d0f320e066651fbc993ea4501b8d3b23ca6c
NathanGarcia <nathangarcia750@gmail.com>
2026-04-20 03:34:15 -0300

    Alteração na Estrutura

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const linkInterruptor = document.querySelector("#interruptor");
+let header = document.querySelector(".header");
 let box = document.querySelector("#checkbox"),
   interruptor = document.querySelector("#interruptor");
-
-
 
 function changeMode() {
   var r = document.querySelectorAll(".change"),
@@ -21,9 +20,14 @@ function changeMode() {
       ? (e.style.backgroundColor = "#000")
       : (e.style.backgroundColor = "#FFF");
   }
+  if (box.checked) {
+    header.style.backgroundColor = "#000"; // ou a cor do tema escuro
+  } else {
+    header.style.backdropFilter = "blur(1.5px)";
+    header.style.backgroundColor = "transparent";
+  }
 }
-interruptor.addEventListener("click", function () {
-  (box.checked = !box.checked), changeMode();
+interruptor.addEventListener("click", function (e) {
+  e.preventDefault();
+  ((box.checked = !box.checked), changeMode());
 });
-
-
